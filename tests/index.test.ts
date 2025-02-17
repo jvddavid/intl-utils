ad8ea789e8f82526c4ed9ce944eb8ad23e80c02c
Joao Victor <j.victordavid2@gmail.com>
2025-02-17 18:11:33 -0300

    adiciona testes para formatação de criptomoedas no IntlPlugin

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,48 @@
-import { describe, expect, it } from 'vitest'
+import { describe, it } from 'vitest'
+import { IntlPlugin } from '../src'
 
 describe(
   'Logger',
   () => {
     it(
-      'should create a new logger',
-      async () => {
-        expect(true).toBeDefined()
-      }
+      'should be format 100.134121 to BTC',
+      async ({
+        expect,
+      }) => {
+        const sut = new IntlPlugin({
+          locale: 'en-US',
+          timeZone: 'America/New_York',
+        })
+
+        const actual = sut.CryptoFormat({
+          style: 'currency',
+          currency: 'BTC',
+          currencyDisplay: 'code',
+          minimumFractionDigits: 6,
+          maximumFractionDigits: 6,
+        }).format(100.134121)
+        expect(actual.startsWith('BTC')).toBeTruthy()
+      },
     )
-  }
+    it (
+      'should be format 100.134121 to BTC with symbol',
+      async ({
+        expect,
+      }) => {
+        const sut = new IntlPlugin({
+          locale: 'en-US',
+          timeZone: 'America/New_York',
+        })
+
+        const actual = sut.CryptoFormat({
+          style: 'currency',
+          currency: 'BTC',
+          currencyDisplay: 'symbol',
+          minimumFractionDigits: 6,
+          maximumFractionDigits: 6,
+        }).format(100.134121)
+        expect(actual.startsWith('₿')).toBeTruthy()
+      },
+    )
+  },
 )
