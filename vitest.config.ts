7df436432836386b3cffd6b46f02c0bf169c8455
Joao Victor <j.victordavid2@gmail.com>
2025-02-17 18:11:27 -0300

    adiciona plugin vite-tsconfig-paths à configuração do Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,6 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    // ...
-  }
+  plugins: [tsconfigPaths()],
 })
