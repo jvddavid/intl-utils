517f573dbc24ce20791e8e272b90eb58586cca9c
Joao Victor <j.victordavid2@gmail.com>
2025-02-17 15:21:17 -0300

    initial files

---
+++ tests/index.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest'
+
+describe(
+  'Logger',
+  () => {
+    it(
+      'should create a new logger',
+      async () => {
+        expect(true).toBeDefined()
+      }
+    )
+  }
+)
